e4517979733ce94afad9849587d7fa66054dc7c0
shin-eunsu <flost@naver.com>
2021-05-03 19:28:26 +0900

    실습1

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
+#include "Add.h"
+
+using namespace std;
+
 int main()
 {
+	int a = 0;
+	int b = 0;
+	int result = 0;
 
+	cout << "정수 2개 입력: ";
+	cin >> a >> b;
+	result = lab1::Add(a, b);
+	cout << "Result : " << result << endl;
+
+	return 0;
+}
+
+namespace lab1
+{
+	int Add(int a, int b)
+	{
+		return a + b;
+	}
 }
