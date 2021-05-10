0f1804ee18a90369d4548b9adbb87e79fcc74aba
Eunsu <flost@naver.com>
2021-05-11 02:43:53 +0900

    (int)c -> static_cast<int>(c)

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,19 +1,25 @@
 #include "Lab2.h"
 
-namespace lab2{    
-    void PrintIntegers(std::istream& in, std::ostream& out){
+namespace lab2
+{    
+    void PrintIntegers(std::istream& in, std::ostream& out)
+    {
         out << setw(12) << "oct" << setw(11) << "dec" << setw(10) << "hex" << endl;
-        out << setw(13) << setfill('-') << " " << setw(11) << " " << setw(9) << "-" << setfill(' ');
+        out << setw(13) << setfill('-') << " " << setw(11) << " " << setw(10) << "-" << setfill(' ');
+                
         int cnt = 0;
         char str[BUFSIZ];
         bool bCheckPrint = false;
         char c;
-        while (in.get(c)){            
-            if (IsInteger(c)){
+        while (in.get(c))
+        {            
+            if (IsInteger(c))
+            {
                 str[cnt++] = c;
                 bCheckPrint = true;
             }
-            else if(bCheckPrint){
+            else if(bCheckPrint)
+            {
                 str[cnt] = '\0';
                 int outValue = atoi(str);
                 out << endl << setw(12) << oct << outValue << setw(11) << dec << outValue << setw(10) << hex << uppercase << outValue ;
@@ -23,37 +29,45 @@ namespace lab2{
                 bCheckPrint = false;
             }
         }
-        if (cnt != 0){
+        if (cnt != 0)
+        {
             str[cnt] = '\0';
             int outValue = atoi(str);
             out << endl <<setw(12) << oct << outValue << setw(11) << dec << outValue << setw(10) << hex << uppercase << outValue;
         }
     }
 
-    void PrintMaxFloat(std::istream& in, std::ostream& out){
+    void PrintMaxFloat(std::istream& in, std::ostream& out)
+    {        
     }
 
-    bool IsInteger(const char c){
-        int val = (int)c;
+    bool IsInteger(const char c)
+    {
+        int val = static_cast<int>(c);
         //DEC 0~9 : ASCII 48~57
-        if (val >= 48 && val <= 57){
+        if (val >= 48 && val <= 57)
+        {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
 
-    bool IsFloat(const char c){
+    bool IsFloat(const char c)
+    {
         bool bReturnVal = false;
         bool bDot = false;
-        int val = (int)c;
+        int val = static_cast<int>(c);
 
         //DEC 0~9 : ASCII 48~57, dot : 46
-        if (val >= 48 && val <= 57 || val == 46){
+        if (val >= 48 && val <= 57 || val == 46)
+        {
             return true;
         }
-        else{
+        else
+        {
         }
 
         return bReturnVal;
