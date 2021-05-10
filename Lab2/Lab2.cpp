573409eb14fbeb73108e240cf6717fd2c896ecc6
Eunsu <flost@naver.com>
2021-05-11 02:14:26 +0900

    PrintIntegers() 완료

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -5,20 +5,20 @@ namespace lab2
     void PrintIntegers(std::istream& in, std::ostream& out)
     {
         out << setw(12) << "oct" << setw(11) << "dec" << setw(10) << "hex" << endl;
-        out << setw(13) << setfill('-') << "." << setw(11) << "." << setw(10) << "." << setfill(' ');
+        out << setw(13) << setfill('-') << " " << setw(11) << " " << setw(10) << " " << setfill(' ');
                 
         int cnt = 0;
         char str[BUFSIZ];
-        bool checkPrint = false;
+        bool bCheckPrint = false;
         char c;
         while (in.get(c))
         {            
             if (IsInteger(c))
             {
                 str[cnt++] = c;
-                checkPrint = true;
+                bCheckPrint = true;
             }
-            else if(checkPrint)
+            else if(bCheckPrint)
             {
                 str[cnt] = '\0';
                 int outValue = atoi(str);
@@ -26,30 +26,46 @@ namespace lab2
                 
                 cnt = 0;
                 memset(str, '\0', BUFSIZ);
-                checkPrint = false;
+                bCheckPrint = false;
             }
         }
         if (cnt != 0)
         {
             str[cnt] = '\0';
             int outValue = atoi(str);
-            out << endl <<setw(12) << oct << outValue << setw(11) << dec << outValue << setw(10) << hex << outValue;
+            out << endl <<setw(12) << oct << outValue << setw(11) << dec << outValue << setw(10) << hex << uppercase << outValue;
         }
     }
 
     void PrintMaxFloat(std::istream& in, std::ostream& out)
-    {
-        
+    {        
     }
 
     bool IsInteger(const char c)
     {
         int val = (int)c;
-
         //DEC 0~9 : ASCII 48~57
         if (val >= 48 && val <= 57)
             return true;
         else
             return false;
+    }
+
+    bool IsFloat(const char c)
+    {
+        bool bReturnVal = false;
+        bool bDot = false;
+        int val = (int)c;
+
+        //DEC 0~9 : ASCII 48~57, dot : 46
+        if (val >= 48 && val <= 57 || val == 46)
+        {
+            return true;
+        }
+        else
+        {
+        }
+
+        return bReturnVal;
     }
 }
