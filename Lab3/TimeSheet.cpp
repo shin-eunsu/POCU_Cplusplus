0db9f3ababe03d6a7348f86c08a83b73702ae382
Eunsu <flost@naver.com>
2021-05-20 00:15:22 +0900

    실습3 중간저장

--- Lab3/TimeSheet.cpp
+++ Lab3/TimeSheet.cpp
@@ -3,35 +3,103 @@
 namespace lab3
 {
 	TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
+		: mNameSize(strlen(name) + 1)
+		, mMaxEntries(maxEntries) // maxEntries : 저장가능 수량
+		, mSaveTime{ '\0' }
+		, mCnt(0)
 	{
+		mName = new char[mNameSize];
+		memcpy(mName, name, mNameSize);
+	}
+
+	TimeSheet::TimeSheet(const TimeSheet& other)
+		: mNameSize(other.mNameSize)
+		, mMaxEntries(other.mMaxEntries)
+		, mSaveTime{ '\0' }
+		, mCnt(0)
+	{
+		mName = new char[mNameSize];
+		memcpy(mName, other.mName, mNameSize);
+	}
+
+	TimeSheet& TimeSheet::operator=(const TimeSheet& rhs)
+	{
+		mNameSize = rhs.mNameSize;
+		mMaxEntries = rhs.mMaxEntries;
+		mCnt = rhs.mCnt;
+		memcpy(mSaveTime, rhs.mSaveTime, BUFSIZ);
+
+		char* tmpName = new char[rhs.mNameSize];
+		memcpy(tmpName, rhs.mName, rhs.mNameSize);
+		delete[] mName;
+		mName = new char[rhs.mNameSize];
+		memcpy(mName, tmpName, rhs.mNameSize);
+		delete[] tmpName;
+
+		return *this;
+	}
+
+	TimeSheet::~TimeSheet()
+	{
+		delete[] mName;
 	}
 
 	void TimeSheet::AddTime(int timeInHours)
 	{
+		if (timeInHours >= minTime && timeInHours <= maxTime)
+		{
+			mSaveTime[mCnt++] = timeInHours;
+		}
 	}
 
 	int TimeSheet::GetTimeEntry(unsigned int index) const
 	{
-		return 0;
+		unsigned int cnt = mCnt;
+		if (index >= cnt)
+		{
+			return -1;
+		}
+		return mSaveTime[index];
 	}
 
 	int TimeSheet::GetTotalTime() const
 	{
-		return 0;
+		int sum = 0;
+		for (int i = 0; i < mCnt; i++)
+		{
+			sum += mSaveTime[mCnt];
+		}
+		return sum;
 	}
 
 	float TimeSheet::GetAverageTime() const
 	{
-		return 0.0f;
+		int totalTime = GetTotalTime();
+		float avg = totalTime / static_cast<float>(mCnt);
+
+		return avg;
 	}
 
 	float TimeSheet::GetStandardDeviation() const
 	{
-		return 0.0f;
+		float std;
+		float var;
+		float avg = GetAverageTime();
+		float sum = 0;
+
+		for (int i = 0; i < mCnt; i++)
+		{
+			sum += powf(static_cast<float>(mSaveTime[mCnt]) - avg, 2);
+		}
+		var = sum / static_cast<float>(mCnt);
+		std = sqrtf(var);
+
+		return std;
 	}
 
 	const std::string& TimeSheet::GetName() const
 	{
-		return "temporary";
+
+		return mName;
 	}
 }
--- Lab3/TimeSheet.h
+++ Lab3/TimeSheet.h
@@ -7,14 +7,24 @@ namespace lab3
 	{
 	public:
 		TimeSheet(const char* name, unsigned int maxEntries);
+		TimeSheet(const TimeSheet& other);
+		~TimeSheet();
 		void AddTime(int timeInHours);
 		int GetTimeEntry(unsigned int index) const;
 		int GetTotalTime() const;
 		float GetAverageTime() const;
 		float GetStandardDeviation() const;
 		const std::string& GetName() const;
+		TimeSheet& operator=(const TimeSheet& rhs);
 
 	private:
-		// 필요에 따라 private 변수와 메서드를 추가하세요.
+		char* mName;
+		int mSaveTime[BUFSIZ];
+		int mCnt;
+		size_t mNameSize ;
+		unsigned int mMaxEntries;
+
+		const int minTime = 1;
+		const int maxTime = 10;
 	};
 }
