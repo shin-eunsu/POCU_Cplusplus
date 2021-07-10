e85a38750b368b5c5d27ef5670c0533bf39ce19c
Eunsu <flost@naver.com>
2021-07-11 00:52:46 +0900

    21.07.11

--- Lab8/FixedBoolVector.h
+++ Lab8/FixedBoolVector.h
@@ -1,1 +1,84 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+namespace lab8
+{
+	template<size_t N>
+	class FixedVector<bool, N>
+	{
+	public:
+		FixedVector()
+			: mSize(0)
+			, mArray{{}}
+		{
+
+		}
+
+		bool Add(bool data)
+		{
+			if (mSize >= N)
+			{
+				return false;
+			}
+
+			mArray[mSize++] = data;
+			return true;
+		}
+
+		bool Remove(bool data)
+		{
+			for (size_t i = 0; i < mSize; i++)
+			{
+				if (mArray[i] == data)
+				{
+					for (size_t j = i; j < mSize - 1; j++)
+					{
+						mArray[j] = mArray[j + 1];
+					}
+					mSize--;
+					return true;
+				}
+			}
+			return false;
+		}
+
+		bool Get(unsigned int index)
+		{
+			return mArray[index];
+		}
+
+		bool operator[](unsigned int index)
+		{
+			return mArray[index];
+		}
+
+		int GetIndex(bool data)
+		{
+			for (size_t i = 0; i < mSize; i++)
+			{
+				if (mArray[i] == data)
+				{
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		size_t GetSize()
+		{
+			return mSize;
+		}
+
+		size_t GetCapacity()
+		{
+			return N;
+		}
+
+	private:
+		size_t mSize;
+		uint32_t mArray[N];
+	};
+
+}
--- Lab8/FixedVector.h
+++ Lab8/FixedVector.h
@@ -1,6 +1,104 @@
 #pragma once
 
-class FixedVector
+namespace lab8
 {
+	template<typename T, size_t N>
+	class FixedVector
+	{
+	public:
+		FixedVector();
+		bool Add(const T& data);
+		bool Remove(const T& data);
+		T& Get(unsigned int index);
+		T& operator[](unsigned int index);
+		T& operator=(const T& data);
+		int GetIndex(const T& data);
+		size_t GetSize();
+		size_t GetCapacity();
 
-};
+	private:
+		size_t mSize;
+		T mArray[N];
+	};
+
+	template<typename T, size_t N>
+	FixedVector<T, N>::FixedVector()
+		: mSize(0)
+		, mArray{{}}
+	{
+
+	}
+
+	template<typename T, size_t N>
+	bool FixedVector<T, N>::Add(const T& data)
+	{
+		if (mSize >= N)
+		{
+			return false;
+		}
+
+		mArray[mSize++] = data;
+		return true;
+	}
+
+	template<typename T, size_t N>
+	bool FixedVector<T, N>::Remove(const T& data)
+	{
+		for (size_t i = 0; i < mSize; i++)
+		{
+			if (mArray[i] == data)
+			{
+				for (size_t j = i; j < mSize - 1; j++)
+				{
+					mArray[j] = mArray[j + 1];
+				}
+				mSize--;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	template<typename T, size_t N>
+	T& FixedVector<T, N>::Get(unsigned int index)
+	{
+		return mArray[index];
+	}
+
+	template<typename T, size_t N>
+	T& FixedVector<T, N>::operator[](unsigned int index)
+	{
+		return mArray[index];
+	}
+
+	template<typename T, size_t N>
+	T& FixedVector<T, N>::operator=(const T& data)
+	{
+		return data;
+	}
+
+	template<typename T, size_t N>
+	int FixedVector<T, N>::GetIndex(const T& data)
+	{
+		for (size_t i = 0; i < mSize; i++)
+		{
+			if (mArray[i] == data)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	template<typename T, size_t N>
+	size_t FixedVector<T, N>::GetSize()
+	{
+		return mSize;
+	}
+
+	template<typename T, size_t N>
+	size_t FixedVector<T, N>::GetCapacity()
+	{
+		return N;
+	}
+}
